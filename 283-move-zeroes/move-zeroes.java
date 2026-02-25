fab5d03b82ff6f4f525c85b611eeeb013bd1031c
Vijaygadi-1706 <vijaygadi48@gmail.com>
2026-02-25 21:50:14 +0530

    Time: 2 ms (90.18%) | Memory: 48.1 MB (5.52%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,15 +1,14 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int insertPos = 0;
-        
-        for (int num : nums) {
-            if (num != 0) {
-                nums[insertPos++] = num;
-            }
+    public void moveZeroes(int[] arr) {
+        int j = 0;
+      for (int i = 0; i < arr.length; i ++){
+        if (arr[i] != 0){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j ++;
         }
         
-        while (insertPos < nums.length) {
-            nums[insertPos++] = 0;
-        }
+      }
     }
 }
