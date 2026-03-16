1689d4632d49e51fa643af0bc10f617a1750b3f5
Vijaygadi-1706 <vijaygadi48@gmail.com>
2026-03-16 09:32:24 +0530

    Time: 4 ms (41.86%) | Memory: 42.9 MB (97.84%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = Map.of(')', '(', '}', '{', ']', '[');
+        
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                char top = stack.isEmpty() ? '#' : stack.pop();
+                if (top != map.get(c)) return false;
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
